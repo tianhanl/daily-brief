e4e8028fd7e86511ffd233d72f08185c5d4edfe1
Tianhang Liu <tianhanl@usc.edu>
2017-06-03 13:48:27 +0800

    Auto stash before revert of "add news card component and register it to news box coponent"

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="news-card">
+    <h2 class="news-name">{{newsNameTitle}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'news-card',
+  props: ['newsName'],
+  computed: {
+    newsNameTitle: function () {
+      return this.newsName.split('-').join(' ');
+      // remember to use this when accessing parameter
+    }
+  }
+}
+</script>
+<style>
+.news-card {
+  border-left: 2px solid;
+  text-align: left;
+  padding: 1em;
+}
+
+.news-name {
+  margin-top: 0;
+  overflow: hidden;
+  text-transform: capitalize;
+}
+</style>
+
+
