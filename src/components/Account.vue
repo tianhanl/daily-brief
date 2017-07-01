97d20aef0723d0fea38086c5f2b04da720edffe6
Tianhang Liu <tianhanl@usc.edu>
2017-07-01 21:40:57 +0800

    modify the style for the login/register components

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="account-box">
-    <nav class="account-select">
+  <div id="account" class="box">
+    <nav class="select">
       <a href="#" @click="showRegister">Register</a>
       <a href="#" @click="showLogin">Log In</a>
     </nav>
@@ -35,9 +35,50 @@ export default {
     }
   }
 }
-
 </script>
 <style>
+.box {
+  padding: 1rem;
+}
 
+.select {
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: space-around;
+}
+
+.select a:hover {
+  transition: all;
+  color: #2C3E50;
+}
+
+#account {
+  margin: auto;
+  max-width: 480px;
+}
+
+#account form {
+  position: relative;
+  overflow: hiddend;
+  margin: auto;
+  padding: 1rem;
+}
+
+#account input {
+  display: block;
+  padding: 0.5rem;
+  margin: 0.5rem auto;
+  width: 80%
+}
+
+#account button {
+  display: block;
+  width: 80%;
+  margin: 1.5rem auto;
+  background: white;
+  padding: 0.5rem;
+  border: solid 2px;
+  border-radius: 4px;
+}
 </style>
 
